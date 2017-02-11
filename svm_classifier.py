c56bd6ed79b9dd5ab9956abe5f9f2cb27bab8501
Adam Aji <adam.m.aji@lmco.com>
2017-02-11 00:40:06 -0500

    Add more comments to classifier

--- svm_classifier.py
+++ svm_classifier.py
@@ -2,7 +2,7 @@
 #
 # runs through a 2-class classification
 # using an SVM with a linear kernel
-# adapted from 
+# adapted from the sklearn example code at
 # http://scikit-learn.org/stable/auto_examples/classification/plot_digits_classification.html
 
 from __future__ import print_function
@@ -15,26 +15,32 @@ FEATURE_DIR = "./color_histogram_features"
 TRAINING_FEATURES_FILE = os.path.join(FEATURE_DIR, "training_features.npy")
 VALIDATION_FEATURES_FILE = os.path.join(FEATURE_DIR, "validation_features.npy")
 TESTING_FEATURES_FILE = os.path.join(FEATURE_DIR, "testing_features.npy")
+BIRD_LABEL = 0
+PLANE_LABEL = 1
 
 if __name__ == "__main__":
 
-    # read features from numpy files
+    # read extracted features from numpy files
     training_data = np.load(TRAINING_FEATURES_FILE)
     validation_data = np.load(VALIDATION_FEATURES_FILE)
     testing_data = np.load(TESTING_FEATURES_FILE)
 
-    training_labels = np.asarray([1] * 1500 + [2] * 1500)
-    validation_labels = np.asarray([1] * 100 + [2] * 100)
-    testing_labels = np.asarray([1] * 400 + [2] * 400)
+    # assign corresponding labels for the images
+    # (e.g., for the training images, we had 1500
+    # bird images prepended to 1500 plane images when
+    # we saved the .npy features in extract_features.py)
+    training_labels = np.asarray([BIRD_LABEL] * 1500 + [PLANE_LABEL] * 1500)
+    validation_labels = np.asarray([BIRD_LABEL] * 100 + [PLANE_LABEL] * 100)
+    testing_labels = np.asarray([BIRD_LABEL] * 400 + [PLANE_LABEL] * 400)
 
-    #classifier = svm.SVC( gamma = 0.001 )
+    # train the default sklearn linear SVM
     classifier = svm.LinearSVC()
     classifier.fit(training_data, training_labels)
 
+    # test the trained SVM
     predictions = classifier.predict(testing_data)
 
+    # show results on the test data
     print("Classification stats for classifier %s:\n%s\n"
           % (classifier, metrics.classification_report(testing_labels, predictions)))
     print("Confusion matrix:\n%s" % metrics.confusion_matrix(testing_labels, predictions))
-
- 
